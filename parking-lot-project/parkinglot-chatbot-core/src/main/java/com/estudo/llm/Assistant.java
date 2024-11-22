df1a39ca9a1f2df409b2260a33d09a35790eb2ea
Romualdo Junior <romualdo.jrr@gmail.com>
2024-11-22 13:59:32 -0300

    implementação de tool e ajuste do assistente

--- parking-lot-project/parkinglot-chatbot-core/src/main/java/com/estudo/llm/Assistant.java
+++ parking-lot-project/parkinglot-chatbot-core/src/main/java/com/estudo/llm/Assistant.java
@@ -1,30 +1,28 @@
 package com.estudo.llm;
 
+import com.estudo.tools.PricingService;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.ApplicationScoped;
 
-@RegisterAiService
 @ApplicationScoped
+@RegisterAiService(tools = {
+        PricingService.class
+})
 public interface Assistant {
 
     @SystemMessage(
             """
-            Você é um assistente de um estacionamento de carros.
+            Você é um assistente de um estacionamento de carros e o seu nome é Alex.
             Você gerencia esse estacionamento consultando as vagas disponiveis, cobrando os clientes pelo tempo em que eles ficam nelas.
             
-            Existe somente 4 vagas no estacionamento, e o cliente pode ficar em qualquer uma delas.
-            O valor é cobrado da seguinte forma:
-            - a primeira hora são 5 reais
-            - demais horas são 2 reais
-            - por dia ou per noite são 50 reais
+            Você deve consultar o preço da primeira hora e o preço das demais horas do estacionamento.
             
             Quando o cliente se comprimentar, você vai se apresentar e vai falar sobre as vagas que temos e os valores.
-            Se o cliente perguntar de algo que esqueceu e estiver no histórico de conversas, você pode responder.
             
-            Qualquer outra coisa que o cliente pergunte, você vai responder que só responde perguntas que foram instruidas para você
+            Se o cliente perguntar de algo que esqueceu e estiver no histórico de conversas, você pode responder.
             
             Não invente nenhuma informação  que não esteja nessas instruções que foram passadas para você
             """
