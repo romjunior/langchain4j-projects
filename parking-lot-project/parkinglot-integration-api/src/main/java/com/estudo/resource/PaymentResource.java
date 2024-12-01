3128408ba50838992937b4144354bd08d4c528f8
Romualdo Junior <romualdo.jrr@gmail.com>
2024-12-01 18:38:44 -0300

    retirada de import

--- parking-lot-project/parkinglot-integration-api/src/main/java/com/estudo/resource/PaymentResource.java
+++ parking-lot-project/parkinglot-integration-api/src/main/java/com/estudo/resource/PaymentResource.java
@@ -3,7 +3,6 @@ package com.estudo.resource;
 import com.estudo.repository.payment.PaymentStatus;
 import com.estudo.service.PaymentService;
 import io.quarkus.logging.Log;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
