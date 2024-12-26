5671e85fba2c5917d6f6c5bcbeb3f80a917b468f
Romualdo Junior <romualdo.jrr@gmail.com>
2024-12-25 23:17:19 -0300

    ajuste prompt guardrail

--- parking-lot-project/parkinglot-chatbot-core/src/main/java/com/estudo/llm/GuardRailsAssistant.java
+++ parking-lot-project/parkinglot-chatbot-core/src/main/java/com/estudo/llm/GuardRailsAssistant.java
@@ -10,14 +10,14 @@ public interface GuardRailsAssistant {
 
 
     @SystemMessage("""
-            Você vai responder entre 0(se o assunto não tem nada haver) até 10 se o assunto tem haver da mensagem '{message}' com a lista de assuntos abaixo listados:
+            Você vai responder entre 0(se o assunto não possui nenhum relacionamento) até 10 se o assunto tem relacionamento da mensagem '{message}' com a lista de assuntos abaixo listados:
             
             Assuntos:
             - alocação de uma vaga do carro
             - Consulta de vaga pela placa do carro
             - Calculo do ticket de alocação
             - Pagamento do ticket de alocação
-            - Comprimentos como 'bom dia', 'boa tarde' e 'boa noite'
+            - Comprimentos como 'bom dia', 'boa tarde', 'boa noite', 'Oi', 'Olá' e 'Olá, tudo bem?'
             - Despedidas como 'tchau', 'adeus' e 'até mais'
             - Agradecimentos como 'obrigado', 'muito obrigado' e 'agradecido'
             """)
